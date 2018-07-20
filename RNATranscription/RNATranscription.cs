e5c2b731776731641d47b6b0b2707f3b44f5bba2
rehman <rehmanmomin@gmail.com>
2018-07-20 09:30:37 +0530

    solved the solution

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -6,7 +6,55 @@ namespace RNATranscriptionModule
     {
         public static string ToRna(string nucleotide)
         {
-            throw new NotImplementedException("You need to implement this function.");
+
+            /*
+             *G -> C
+            C -> G
+            T -> A
+            A -> U
+            */
+
+            char[] arr = nucleotide.ToCharArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                switch (arr[i])
+                {
+                    case 'A':
+                        arr[i] = 'U';
+                        break;
+                    case 'T':
+                        arr[i] = 'A';
+                        break;
+                    case 'C':
+                        arr[i] = 'G';
+                        break;
+                    case 'G':
+                        arr[i] = 'C';
+                        break;
+                }
+            }
+
+            return new string(arr);
+
+
+
+            //string s = nucleotide.Replace('A', 'U');
+            //s = s.Replace('T', 'A');
+            //s = s.Replace('C', 'G');
+            //s = s.Replace('G', 'C');
+
+            //foreach (char c in arr)
+            //{
+            //    switch (c)
+            //    {
+            //        case 'G':
+            //        default:
+            //            break;
+            //    }
+            //}
+            
+
         }
     }
 }
